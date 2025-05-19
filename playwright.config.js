b0f64e0b7c85827eb12e91403e9b3d0e86c3556e
bichvt233 <bich_vt@flinters.vn>
2025-05-19 13:27:05 +0700

    thay doi de luu ket qua test

--- playwright.config.js
+++ playwright.config.js
@@ -1,6 +1,12 @@
+// playwright.config.js
 const { defineConfig } = require('@playwright/test');
 
 module.exports = defineConfig({
+  use: {
+    headless: true, // bắt buộc trên GitHub Actions
+    screenshot: 'on',      // chụp ảnh nếu lỗi
+    video: 'on' // quay video nếu fail hoặc retry
+  },
   projects: [
     { name: 'Chromium', use: { browserName: 'chromium' } },
     { name: 'Firefox', use: { browserName: 'firefox' } },
